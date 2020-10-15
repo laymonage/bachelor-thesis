df3d47ff5a4d351b6667a1b3e2cf3e95cac2307f
sage <laymonage@gmail.com>
2020-10-15 15:16:34 +0700

    write: Path transforms

--- codes/3-keyexistence.py
+++ codes/3-keyexistence.py
@@ -1,6 +1,6 @@
 >>> Dog.objects.filter(data__has_key='breed')
 <QuerySet [<Dog: Rufus>, <Dog: Meg>, <Dog: Cooper>]>
->>> Dog.objects.filter(data__has_keys=['breed', 'owner'])
-<QuerySet [<Dog: Rufus>, <Dog: Meg>]>
->>> Dog.objects.filter(data__has_any_keys=['breed', 'age'])
+>>> Dog.objects.filter(data__has_keys=['breed', 'favorite_toys'])
+<QuerySet [<Dog: Cooper>]>
+>>> Dog.objects.filter(data__has_any_keys=['breed', 'age', 'favorite_toys'])
 <QuerySet [<Dog: Rufus>, <Dog: Meg>, <Dog: Cooper>]>
