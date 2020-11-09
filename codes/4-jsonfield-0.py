0721fadf5761b1e60925a95944c54e6a20c707ec
sage <laymonage@gmail.com>
2020-11-09 20:32:58 +0700

    write: Initial paragraphs for chapter 4

---
+++ codes/4-jsonfield-0.py
@@ -0,0 +1,19 @@
+class JSONField(CheckFieldDefaultMixin, Field):
+    empty_strings_allowed = False
+    description = _('A JSON object')
+    default_error_messages = {
+        'invalid': _('Value must be valid JSON.'),
+    }
+    _default_hint = ('dict', '{}')
+
+    def __init__(
+        self, verbose_name=None, name=None, encoder=None, decoder=None,
+        **kwargs,
+    ):
+        if encoder and not callable(encoder):
+            raise ValueError('The encoder parameter must be a callable object.')
+        if decoder and not callable(decoder):
+            raise ValueError('The decoder parameter must be a callable object.')
+        self.encoder = encoder
+        self.decoder = decoder
+        super().__init__(verbose_name, name, **kwargs)
