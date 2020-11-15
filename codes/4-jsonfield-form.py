b71f2423f5109fe26c641421532ca5517f444e5d
sage <laymonage@gmail.com>
2020-11-15 14:57:38 +0700

    write: JSONField formfield method

---
+++ codes/4-jsonfield-form.py
@@ -0,0 +1,10 @@
+class JSONField(Field):
+    ...
+
+    def formfield(self, **kwargs):
+        return super().formfield(**{
+            'form_class': forms.JSONField,
+            'encoder': self.encoder,
+            'decoder': self.decoder,
+            **kwargs,
+        })
