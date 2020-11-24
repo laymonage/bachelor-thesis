b82c35ab3253a84cfc5aed474e5fa196b1f88cef
sage <laymonage@gmail.com>
2020-11-24 18:24:30 +0700

    rev: Revise fourth chapter to use Django 3.1.0 as the basis

---
+++ codes/4-selectformat.py
@@ -0,0 +1,10 @@
+class JSONField(CheckFieldDefaultMixin, Field):
+    ...
+
+    def select_format(self, compiler, sql, params):
+        if (
+            compiler.connection.features.has_native_json_field and
+            self.decoder is not None
+        ):
+            return compiler.connection.ops.json_cast_text_sql(sql), params
+        return super().select_format(compiler, sql, params)
