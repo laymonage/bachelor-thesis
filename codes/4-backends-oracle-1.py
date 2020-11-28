269a8017e3a1d92fd5cb14b2aa01654a8545c592
sage <laymonage@gmail.com>
2020-11-28 23:29:43 +0700

    rev: Minor revision for the previous chapters

--- codes/4-backends-oracle-1.py
+++ codes/4-backends-oracle-1.py
@@ -12,13 +12,3 @@ class DatabaseWrapper(BaseDatabaseWrapper):
         'JSONField': '%(qn_column)s IS JSON',
         ...
     }
-
-# django.db.backends.oracle.operations
-class DatabaseOperations(BaseDatabaseOperations):
-    ...
-    def get_db_converters(self, expression):
-        converters = super().get_db_converters(expression)
-        internal_type = expression.output_field.get_internal_type()
-        if internal_type in ['JSONField', 'TextField']:
-            converters.append(self.convert_textfield_value)
-        ...
