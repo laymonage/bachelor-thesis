8b35698172f95dc9c730e30b3669394bc253176a
sage <laymonage@gmail.com>
2020-11-30 10:59:38 +0700

    write: JSONField checks

---
+++ codes/4-jsonfield-check.py
@@ -0,0 +1,8 @@
+class JSONField(CheckFieldDefaultMixin, Field):
+    ...
+
+    def check(self, **kwargs):
+        errors = super().check(**kwargs)
+        databases = kwargs.get('databases') or []
+        errors.extend(self._check_supported(databases))
+        return errors
