be4e8ad475086267b9cba18bbee8977fbbc5902a
sage <laymonage@gmail.com>
2020-11-30 10:59:38 +0700

    write: JSONField checks

---
+++ codes/4-jsonfield-checksupported.py
@@ -0,0 +1,18 @@
+def _check_supported(self, databases):
+    errors = []
+    for db in databases:
+        if not router.allow_migrate_model(db, self.model):
+            continue
+        connection = connections[db]
+        if not (
+            'supports_json_field' in self.model._meta.required_db_features or
+            connection.features.supports_json_field
+        ):
+            errors.append(
+                checks.Error(
+                    '%s does not support JSONFields.'
+                    % connection.display_name,
+                    obj=self.model, id='fields.E180',
+                )
+            )
+    return errors
