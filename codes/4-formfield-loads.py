aa764473e7f734f73f302223b8701e89b44c3dbf
sage <laymonage@gmail.com>
2020-11-30 11:05:58 +0700

    rev: Do not show the class header when showing methods

--- codes/4-formfield-loads.py
+++ codes/4-formfield-loads.py
@@ -1,21 +1,18 @@
-class JSONField(CharField):
+def to_python(self, value):
+    ...
+    try:
+        converted = json.loads(value, cls=self.decoder)
+    except json.JSONDecodeError:
+        raise ValidationError(
+            self.error_messages['invalid'],
+            code='invalid',
+            params={'value': value},
+        )
     ...
 
-    def to_python(self, value):
-        ...
-        try:
-            converted = json.loads(value, cls=self.decoder)
-        except json.JSONDecodeError:
-            raise ValidationError(
-                self.error_messages['invalid'],
-                code='invalid',
-                params={'value': value},
-            )
-        ...
-
-    def bound_data(self, data, initial):
-        ...
-        try:
-            return json.loads(data, cls=self.decoder)
-        except json.JSONDecodeError:
-            return InvalidJSONInput(data)
+def bound_data(self, data, initial):
+    ...
+    try:
+        return json.loads(data, cls=self.decoder)
+    except json.JSONDecodeError:
+        return InvalidJSONInput(data)
