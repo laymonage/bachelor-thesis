8743e32f9aee932adc5e42e8dd860e8519579bfa
sage <laymonage@gmail.com>
2020-12-05 21:03:42 +0700

    write: PostgresOperatorLookup

---
+++ codes/4-pgop-lookup.py
@@ -0,0 +1,9 @@
+class PostgresOperatorLookup(FieldGetDbPrepValueMixin, Lookup):
+    """Lookup defined by operators on PostgreSQL."""
+    postgres_operator = None
+
+    def as_postgresql(self, compiler, connection):
+        lhs, lhs_params = self.process_lhs(compiler, connection)
+        rhs, rhs_params = self.process_rhs(compiler, connection)
+        params = tuple(lhs_params) + tuple(rhs_params)
+        return '%s %s %s' % (lhs, self.postgres_operator, rhs), params
