8504e744e82d5f25b6760014679c48af35d74f4a
sage <laymonage@gmail.com>
2020-12-08 01:00:02 +0700

    write: Transforms closing

---
+++ codes/4-keytransformfactory.py
@@ -0,0 +1,7 @@
+class KeyTransformFactory:
+
+    def __init__(self, key_name):
+        self.key_name = key_name
+
+    def __call__(self, *args, **kwargs):
+        return KeyTransform(self.key_name, *args, **kwargs)
