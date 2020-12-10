b636f3deaec96f85024011bccb0d6b5d29caa312
sage <laymonage@gmail.com>
2020-12-10 14:01:22 +0700

    write: KeyTransformIsNull

---
+++ codes/4-lookups-keytransformisnull.py
@@ -0,0 +1,10 @@
+class KeyTransformIsNull(lookups.IsNull):
+    def as_oracle(self, compiler, connection):
+        if not self.rhs:
+            return HasKey(self.lhs.lhs, self.lhs.key_name).as_oracle(compiler, connection)
+        return super().as_sql(compiler, connection)
+
+    def as_sqlite(self, compiler, connection):
+        if not self.rhs:
+            return HasKey(self.lhs.lhs, self.lhs.key_name).as_sqlite(compiler, connection)
+        return super().as_sql(compiler, connection)
