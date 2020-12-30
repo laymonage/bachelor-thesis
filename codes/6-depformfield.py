041ae79969ed1e64b7ba49d48d5f50b52ba704da
sage <laymonage@gmail.com>
2020-12-30 10:47:50 +0700

    write: Complete chapter 6

---
+++ codes/6-depformfield.py
@@ -0,0 +1,8 @@
+class JSONField(BuiltinJSONField):
+    def __init__(self, *args, **kwargs):
+        warnings.warn(
+            'django.contrib.postgres.forms.JSONField is deprecated in favor '
+            'of django.forms.JSONField.',
+            RemovedInDjango40Warning, stacklevel=2,
+        )
+        super().__init__(*args, **kwargs)
