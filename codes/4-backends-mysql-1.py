e230ce5b3d3996983ee4098d9c8176c9eaedc9d7
sage <laymonage@gmail.com>
2021-01-01 15:32:09 +0700

    rev: Revise chapter 4

--- codes/4-backends-mysql-1.py
+++ codes/4-backends-mysql-1.py
@@ -11,7 +11,9 @@ class DatabaseWrapper(BaseDatabaseWrapper):
     @cached_property
     def data_type_check_constraints(self):
         if self.features.supports_column_check_constraints:
-            check_constraints = { ... }
+            check_constraints = {
+                ...
+            }
             if self.mysql_is_mariadb and self.mysql_version < (10, 4, 3):
                 check_constraints['JSONField'] = 'JSON_VALID(`%(column)s`)'
             return check_constraints
